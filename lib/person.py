e9b240b694075e139957a5b4826883fd76d15b20
Sebastian Martinez <isaacsoccer235@gmail.com>
2023-07-05 21:18:50 -0500

    instances

--- lib/person.py
+++ lib/person.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 class Person:
-    # Class body goes here
 
-    #Instance method definition
-    pass
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
+
+tim = Person()
+
+tim.talk()
+tim.walk()
+    
